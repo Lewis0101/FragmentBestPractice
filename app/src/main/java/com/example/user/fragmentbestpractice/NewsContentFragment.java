f9cace87c212f724812f2395cb2314bfa955fecd
LiuYang <1005859278@qq.com>
2018-10-10 19:52:52 +0800

    完善项目

--- app/src/main/java/com/example/user/fragmentbestpractice/NewsContentFragment.java
+++ app/src/main/java/com/example/user/fragmentbestpractice/NewsContentFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * @author : LiuYang
@@ -33,4 +36,6 @@ public class NewsContentFragment extends Fragment {
         newsContentText.setText(newsContent);//刷新新闻的内容
 
     }
+
+
 }
